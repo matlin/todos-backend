0490969995ac9ad171a07c5d4622454f21a15172
Matthew Linkous <matthew.linkous@gmail.com>
2021-10-28 14:51:58 -0400

    Use aggregators in views

--- src/index.agent.ts
+++ src/index.agent.ts
@@ -1,4 +1,4 @@
-import { Agent } from '@aspen.cloud/agent-typings';
+import { Agent, Log } from '@aspen.cloud/agent-typings';
 
 interface Todo {
   id: number;
@@ -11,39 +11,65 @@ interface Todo {
 const agent: Agent = {
   name: 'Simple Todos',
   sourceId: 'todos',
-  views: {
-    incomplete: async (params, aspen) => {
-      const all = await aspen.getView('all', params);
-      return all.filter((todo) => !todo.done);
+  aggregations: {
+    todosInList: {
+      initialize: (todoList?: Todo[]) => {
+        return new Map(todoList?.map((todo) => [todo.id, todo]));
+      },
+      reducer: (todos: Map<number, Todo>, event: Log<any, any>) => {
+        if (event.data.type === 'todo') {
+          const todo: Todo = {
+            id: event.data.id,
+            message: event.data.message || '',
+            createdAt: event.inserted_at.toDateString(),
+            done: false,
+            listId: event.data.listId,
+          };
+          todos.set(todo.id, todo);
+        }
+        if (event.data.type === 'status_change' && todos) {
+          const todo = todos.get(event.data.todoId);
+          if (todo) {
+            todo.done = event.data.isDone;
+          }
+        }
+        return todos;
+      },
+      serialize: (todoMap: Map<number, Todo>) => {
+        return Array.from(todoMap.values());
+      },
     },
-    all: async ({ list }, aspen) => {
-      const todos: Todo[] = [];
-      await aspen.processEvents(
-        (event) => {
-          if (event.data.type === 'todo') {
-            const todo = {
-              id: event.data.id,
-              message: event.data.message || '',
-              createdAt: event.inserted_at,
-              done: false,
-              listId: event.data.listId,
-            };
-            todos.push(todo);
-          }
-          if (event.data.type === 'status_change' && todos) {
-            const todo = todos.find((todo) => todo.id == event.data.todoId);
-            if (todo) {
-              todo.done = event.data.isDone;
-            }
-          }
-        },
-        { list },
-      );
-      return todos;
+    lists: {
+      initialize: (lists?: string[]) => new Set(lists),
+      reducer: (listSet: Set<string>, event) => {
+        if (event.data.type === 'todo') {
+          listSet.add(event.data.listId);
+        }
+        return listSet;
+      },
+      serialize: (listSet: Set<string>) => Array.from(listSet.values()),
     },
   },
-  overviews: {
-    test: 'test',
+  views: {
+    lists: async (params, aspen) => {
+      return await aspen.getAggregation('lists', { range: 'continuous' });
+    },
+    incomplete: async ({ list }, aspen) => {
+      const todos = await aspen.getAggregation('todosInList', {
+        tags: { list },
+        range: 'continuous',
+      });
+
+      return todos.filter((todo) => !todo.done);
+    },
+    all: async ({ list }, aspen) => {
+      const todos = await aspen.getAggregation('todosInList', {
+        tags: { list },
+        range: 'continuos',
+      });
+
+      return todos;
+    },
   },
   actions: {
     add: async ({ message, list }, aspen) => {
